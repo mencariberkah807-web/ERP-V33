c65dc78e818fa466d056418074bf8d6a588c1c1d
mencariberkah <mencariberkah807@gmail.com>
2026-09-14 11:23:18 +0700

    feat(work-order): implement complete production use case

--- src/application/work-orders/completeProduction.js
+++ src/application/work-orders/completeProduction.js
@@ -1,1 +1,6 @@
-// Use-case boundary.
+import { workOrderRepository } from "../../repositories/workOrders/workOrderRepository.js";
+
+export async function completeProduction({ woNumber, idempotencyKey } = {}) {
+  if (!woNumber) throw new Error("Work Order wajib diisi.");
+  return workOrderRepository.complete(woNumber, idempotencyKey);
+}
