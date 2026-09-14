80f5b2ce80cb1f4a2ac41623fdcc1e36ef4689e7
mencariberkah <mencariberkah807@gmail.com>
2026-09-14 11:23:14 +0700

    feat(work-order): implement start production use case

--- src/application/work-orders/startProduction.js
+++ src/application/work-orders/startProduction.js
@@ -1,1 +1,6 @@
-// Use-case boundary.
+import { workOrderRepository } from "../../repositories/workOrders/workOrderRepository.js";
+
+export async function startProduction({ woNumber, idempotencyKey } = {}) {
+  if (!woNumber) throw new Error("Work Order wajib diisi.");
+  return workOrderRepository.start(woNumber, idempotencyKey);
+}
